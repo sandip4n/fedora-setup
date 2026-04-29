b0ffece24befc06e0ebe7dbfb3598571f4fa8ad4
Sandipan Das <sandipan.osd@gmail.com>
2026-04-29 21:47:30 +0530

    setup-usr: use cascadia code font
    Change the font from Fira Code to Cascadia Code.
    
    Signed-off-by: Sandipan Das <sandipan.osd@gmail.com>

--- setup-usr.sh
+++ setup-usr.sh
@@ -22,11 +22,11 @@ setup_usr__desktop() {
 	gsettings set org.gnome.desktop.interface clock-format "24h"
 	gsettings set org.gnome.desktop.interface clock-show-weekday true
 	gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-	rpm -q --quiet fira-code-fonts && gsettings set org.gnome.desktop.interface document-font-name "Fira Code Regular 11"
-	rpm -q --quiet fira-code-fonts && gsettings set org.gnome.desktop.interface font-name "Fira Code Regular 11"
+	rpm -q --quiet cascadia-code-fonts && gsettings set org.gnome.desktop.interface document-font-name "Cascadia Code Regular 11"
+	rpm -q --quiet cascadia-code-fonts && gsettings set org.gnome.desktop.interface font-name "Cascadia Code Regular 11"
 	rpm -q --quiet gnome-themes-extra && gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 	rpm -q --quiet papirus-icon-theme && gsettings set org.gnome.desktop.interface icon-theme "Papirus"
-	rpm -q --quiet fira-code-fonts && gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code Medium 11"
+	rpm -q --quiet cascadia-code-fonts && gsettings set org.gnome.desktop.interface monospace-font-name "Cascadia Code Regular 11"
 	gsettings set org.gnome.desktop.interface scaling-factor 1
 	gsettings set org.gnome.desktop.interface text-scaling-factor "1.0"
 
